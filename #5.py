68f57ca1aff354318e7586964a5de09312ea110b
Kuznetsova-28 <61417891+Kuznetsova-28@users.noreply.github.com>
2020-04-29 22:03:03 +0500

    Update #5.py

--- #5.py
+++ #5.py
@@ -1,8 +1,8 @@
-> > > > > > p = 2048
-> > > > > kb = float (p) / float(1024)
-> > > > > mb = float (kb) / float(1024)
-> > > > > > печать(КБ)
+>>> p = 2048
+>>> kb = float (p) / float(1024)
+>>> mb = float (kb) / float(1024)
+>>> print(kb)
 2.0
-> > > > > > печать(Мб)
+>>> print(mb)
 0.001953125
 >>> 
