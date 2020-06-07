47e8186f6a79fffe4321c2a1598f2d425e53e17f
Kuznetsova-28 <61417891+Kuznetsova-28@users.noreply.github.com>
2020-06-07 23:01:11 +0500

    Create 23.py

---
+++ 23.py
@@ -0,0 +1,15 @@
+Даны двещественные числа X и Y . Вычислить Z. Z = √(X x Y) при X > Y, Z = ln(X + Y ) в противном случае.
+
+
+import random
+import math
+X = random.randint(1,28)
+Y = random.randint (1,43)
+print("X - ", X)
+print("Y - ", Y)
+if X > Y  :
+    Z = math.sqrt(X*Y)
+    print("Zqrt - ", Z)
+else:
+    Z = math.log(X+Y)
+    print("Zlog - ", Z)
